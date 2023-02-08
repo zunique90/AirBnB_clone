49e9732eb487c36209d6ad6f8b4d81dbfd48e889
Joseph Jeremiah <jerrycul2001@yahoo.com>
2023-02-08 21:19:34 +0000

    Implement create command for the console

--- console.py
+++ console.py
@@ -2,6 +2,7 @@
 # This is the entry point to the console made for the AirBnB clone
 
 import cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -26,6 +27,24 @@ class HBNBCommand(cmd.Cmd):
     def help_EOF(self):
         """Prints the helper test for the EOF command"""
         print("Quit command to exit the program")
+
+    def do_create(self, args):
+        """Creates a new instance of BaseModel
+        saves it (to the JSON file) and prints the id"""
+        if not args:
+            print("** class name missing **")
+        elif args != 'BaseModel':
+            print(BaseModel.__class__.__name__)
+            print("** class doesn't exist **")
+        else:
+            model = BaseModel()
+            model.save()
+            print(model.id)
+
+    def help_create(self):
+        """Prints the helper test for the create command"""
+        print("Creates a new instance of BaseModel, saves it",
+        "(to the JSON file) and prints the id")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
