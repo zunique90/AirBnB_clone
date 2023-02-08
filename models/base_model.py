9dcd8d46b21eabfbd92614e03ad01bb5033b9f1b
Joseph Jeremiah <jerrycul2001@yahoo.com>
2023-02-08 14:36:37 +0000

    Update init method for BaseModel to create an instance from **kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -5,10 +5,26 @@ from datetime import datetime
 import uuid
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    """The base model class for my models"""
+
+    def __init__(self, *args, **kwargs):
+        """Method that initializes the object"""
+        self.__set_attributes(kwargs)
+
+    def __set_attributes(self, attr_dict):
+        """Sets the attributes of a model instance based on **kwargs or default values"""
+        if 'id' not in attr_dict:
+            attr_dict['id'] = str(uuid.uuid4())
+        for attr in ['created_at', 'updated_at']:
+            if attr not in attr_dict:
+                attr_dict[attr] = datetime.now()
+            elif not isinstance(attr_dict[attr], datetime):
+                attr_dict[attr] = datetime.strptime(
+                    attr_dict[attr], "%Y-%m-%dT%H:%M:%S.%f")
+        for attr, value in attr_dict.items():
+            if attr == '__class__':
+                continue
+            setattr(self, attr, value)
 
     def __str__(self):
         """The string representation of the object"""
